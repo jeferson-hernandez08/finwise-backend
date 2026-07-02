50a66bb287a9f28c81b1d9fcb14d36c6e48c69fd
Jeferson <jefer.hernandez1@gmail.com>
2026-07-01 20:04:52 -0500

    Type Feat: Instaled mongoose and connect data base mongo compas in backend

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb+srv://jeferson_hernandez_finwiseapp:ASDasd.123@cluster0.ii0kln6.mongodb.net/?appName=Cluster0'),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
-
-// Init commit  
