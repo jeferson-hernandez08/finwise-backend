6006040c55f80c34fef3f0a7452585a2c097e1c5
Jeferson <jefer.hernandez1@gmail.com>
2026-07-07 21:03:26 -0500

    Type Feat: Continued create schema user

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,18 @@
+// src/app.module.ts
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { User, UserSchema } from './schemas/user.schema';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb+srv://jeferson_hernandez_finwiseapp:ASDasd.123@cluster0.ii0kln6.mongodb.net/?appName=Cluster0'),
+    MongooseModule.forRoot('mongodb+srv://jeferson_hernandez_finwiseapp:ASDasd.123@cluster0.ii0kln6.mongodb.net/finwise_db?retryWrites=true&w=majority'),
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
-
-// Commit 
