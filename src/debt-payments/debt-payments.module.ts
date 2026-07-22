4a3efa4fea60cccc8c9300e4ce9df5d96ded0e7c
Jeferson Hernandez <jefer.hernandez1@gmail.com>
2026-07-21 21:27:04 -0500

    FW-07 | Type Feat: Init add debt-payments module with CRUD

--- src/debt-payments/debt-payments.module.ts
+++ src/debt-payments/debt-payments.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { DebtPaymentsService } from './debt-payments.service';
 import { DebtPaymentsController } from './debt-payments.controller';
+import { DebtPayment, DebtPaymentSchema } from '../schemas/debt-payment.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: DebtPayment.name, schema: DebtPaymentSchema }
+    ])
+  ],
   controllers: [DebtPaymentsController],
   providers: [DebtPaymentsService],
 })
