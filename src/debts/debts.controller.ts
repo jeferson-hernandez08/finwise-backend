fc9d5d6301985a5ef9902843c3d3e8ebbaacf4ae
Jeferson Hernandez <jefer.hernandez1@gmail.com>
2026-08-19 18:21:56 -0500

    FW-13 | Type Feat: JwtAuthGuard route security is implemented for the debts table.

--- src/debts/debts.controller.ts
+++ src/debts/debts.controller.ts
@@ -1,40 +1,43 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, HttpCode, HttpStatus } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, HttpCode, HttpStatus, UseGuards } from '@nestjs/common';
 import { DebtsService } from './debts.service';
 import { CreateDebtDto } from './dto/create-debt.dto';
 import { UpdateDebtDto } from './dto/update-debt.dto';
+import { UserId } from '../decorators/user-id.decorator';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
 @Controller('debts')
+@UseGuards(JwtAuthGuard) // Protege todas las rutas
 export class DebtsController {
   constructor(private readonly debtsService: DebtsService) {}
 
   @Post()
-  create(@Body() createDebtDto: CreateDebtDto) {
+  create(@Body() createDebtDto: CreateDebtDto, @UserId() userId: string) {
+    createDebtDto.user_id = userId; // Asignar user_id desde el token
     return this.debtsService.create(createDebtDto);
   }
 
   @Get()
-  findAll() {
-    return this.debtsService.findAll();
+  findAll(@UserId() userId: string) {
+    return this.debtsService.findAllByUser(userId);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.debtsService.findOne(id);
-  }
-
-  @Get('user/:userId')
-  findByUser(@Param('userId') userId: string) {
-    return this.debtsService.findByUser(userId);
+  findOne(@Param('id') id: string, @UserId() userId: string) {
+    return this.debtsService.findOneForUser(id, userId);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateDebtDto: UpdateDebtDto) {
-    return this.debtsService.update(id, updateDebtDto);
+  update(
+    @Param('id') id: string,
+    @Body() updateDebtDto: UpdateDebtDto,
+    @UserId() userId: string,
+  ) {
+    return this.debtsService.updateForUser(id, updateDebtDto, userId);
   }
 
   @Delete(':id')
   @HttpCode(HttpStatus.NO_CONTENT)
-  remove(@Param('id') id: string) {
-    return this.debtsService.remove(id);
+  remove(@Param('id') id: string, @UserId() userId: string) {
+    return this.debtsService.removeForUser(id, userId);
   }
 }
